14b4360edb374d2febf6f7d2f4f6ea99a24ecec4
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-11-11 23:01:15 +0530

    evevR

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
+require('dotenv').config();
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
